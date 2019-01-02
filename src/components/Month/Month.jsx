a752dd28f3e1162ba8d845b63edeb1fadc0c81f3
corontorr <corontorrhome@gmail.com>
2019-01-02 13:25:27 +0200

    minor code cleanings

--- src/components/Month/Month.jsx
+++ src/components/Month/Month.jsx
@@ -3,30 +3,25 @@ import React from 'react';
 import NiceDate from '../../NiceDate';
 import CalendarTile from '../CalendarTile';
 
-const Month = ({ monthObj, todos, onTodoChange }) => {
+const Month = ({ monthObj, todos, ...todoEvents }) => {
   const monthTiles = [
     ...Array(6).fill([...Array(7).keys()].map((elem) => elem + 1)),
   ].map((week, idxW) =>
     week.map((day) => {
-      const tetha = NiceDate.newDate(monthObj);
-      tetha.addDays(day - monthObj.day + 7 * idxW);
+      const tetha = NiceDate.newDate(monthObj, day - monthObj.day + 7 * idxW);
 
-      const isFromOtherMonth = tetha.month !== monthObj.month;
       const dayInTodos = todos.find((el) => el.date.show() === tetha.show());
-      let activities = [];
-      if (dayInTodos) {
-        activities = dayInTodos.activities;
-      }
+
       return (
         <CalendarTile
           dayObj={tetha}
-          activities={activities}
-          isFromOtherMonth={isFromOtherMonth}
-          key={tetha.show()}
-          onTodoChange={onTodoChange}
+          activities={dayInTodos ? dayInTodos.activities : []}
+          isFromOtherMonth={tetha.month !== monthObj.month}
+          key={`tile-${tetha.show()}`}
+          {...todoEvents}
         />
       );
-    }),
+    })
   );
 
   return <div className="month">{monthTiles}</div>;
